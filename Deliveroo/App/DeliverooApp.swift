e821958877589c4f342aa20bf040691f2b08b414
Leoan Ranjit <leoanranjit1991@gmail.com>
2026-02-26 23:54:34 +1100

    re commit app starting point

--- Deliveroo/App/DeliverooApp.swift
+++ Deliveroo/App/DeliverooApp.swift
@@ -11,6 +11,24 @@ import SwiftUI
 struct DeliverooApp: App {
     var body: some Scene {
         WindowGroup {
+            OrderListView(
+                viewModel: OrderListViewModel(
+                    repository: previewRepository
+                )
+            )
         }
+    }
+
+    private var previewRepository: OrderRepository {
+        MockOrderRepository(
+            behavior: .delayedSuccess(
+                [
+                    Order(id: UUID(), title: "Order #1", status: .pending),
+                    Order(id: UUID(), title: "Order #2", status: .inTransit),
+                    Order(id: UUID(), title: "Order #3", status: .delivered)
+                ],
+                delay: 2.0
+            )
+        )
     }
 }

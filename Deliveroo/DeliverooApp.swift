ee13a98936a82e2745acfe0860326a40e645d7f3
Leoan Ranjit <leoanranjit1991@gmail.com>
2026-02-26 23:08:37 +1100

    Initial

---
+++ Deliveroo/DeliverooApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliverooApp.swift
+//  Deliveroo
+//
+//  Created by Aarya Rajbhandari on 26/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct DeliverooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
